3894f8e8669a9796ad31b727059a4351793cb765
김은총 <monmon867867@gmail.com>
2022-11-14 09:30:44 +0900

    실행 파일 실행하기

---
+++ Practice_C/Practice_C/소스.cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h> // C 표준 입출력 헤더파일
+#include <stdlib.h> // C 표준 유틸리티 함수들을 모아놓은 헤더파일
+
+int main(void) // main 함수가 종료될 때 정수형 값을 리턴하겠다는 뜻
+{
+	printf("Be happy!"); // Be happy! 출력
+	system("pause"); // 일시정지
+
+	return 0; // 해당 함수를 정상적으로 끝내고서 운영체제에게 기술한 값을 반환하고 현재 실행 중인 함수를 끝낸다는 뜻
+}
