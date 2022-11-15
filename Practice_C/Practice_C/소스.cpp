180f892cba60a9b670be3e5050f26119e399b81a
김은총 <monmon867867@gmail.com>
2022-11-15 09:02:37 +0900

    정수 자료형의 크기

--- Practice_C/Practice_C/소스.cpp
+++ Practice_C/Practice_C/소스.cpp
@@ -3,12 +3,11 @@
 
 int main(void) // main 함수가 종료될 때 정수형 값을 리턴하겠다는 뜻
 {
-	// 3.14e-5를 소수점 형태로 소수점 이하 7자리까지 출력
-	printf("%.7lf\n", 3.14e-5); // 지수 표기법으로 표기된 값의 실제 값
-	// 0.0000314를 지수 형태로 출력
-	printf("%le\n", 0.0000314); // 지수 표기법으로 표기된 값
-	// 0.0000314를 지수 형태로 소수점 이하 둘째 자리까지 출력
-	printf("%.2le\n", 0.0000314); // 지수 표기법으로 표기된 값
+	printf("long long형의 크기 : %d바이트\n", sizeof(char)); // 1 바이트
+	printf("long long형의 크기 : %d바이트\n", sizeof(short)); // 2 바이트
+	printf("long long형의 크기 : %d바이트\n", sizeof(int)); // 4 바이트
+	printf("long long형의 크기 : %d바이트\n", sizeof(long)); // 4 바이트
+	printf("long long형의 크기 : %d바이트\n", sizeof(long long)); // 8 바이트 
 
 	return 0; // 해당 함수를 정상적으로 끝내고서 운영체제에게 기술한 값을 반환하고 현재 실행 중인 함수를 끝낸다는 뜻
 }
