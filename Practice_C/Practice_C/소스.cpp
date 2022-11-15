e2d6a068efd31f3f4db0a0e2791900997c387400
김은총 <monmon867867@gmail.com>
2022-11-15 09:25:03 +0900

    정수형 변수의 크기 수정

--- Practice_C/Practice_C/소스.cpp
+++ Practice_C/Practice_C/소스.cpp
@@ -3,10 +3,10 @@
 
 int main(void) // main 함수가 종료될 때 정수형 값을 리턴하겠다는 뜻
 {
-	printf("long long형의 크기 : %d바이트\n", sizeof(char)); // 1 바이트
-	printf("long long형의 크기 : %d바이트\n", sizeof(short)); // 2 바이트
-	printf("long long형의 크기 : %d바이트\n", sizeof(int)); // 4 바이트
-	printf("long long형의 크기 : %d바이트\n", sizeof(long)); // 4 바이트
+	printf("char형의 크기 : %d바이트\n", sizeof(char)); // 1 바이트
+	printf("short형의 크기 : %d바이트\n", sizeof(short)); // 2 바이트
+	printf("int형의 크기 : %d바이트\n", sizeof(int)); // 4 바이트
+	printf("long형의 크기 : %d바이트\n", sizeof(long)); // 4 바이트
 	printf("long long형의 크기 : %d바이트\n", sizeof(long long)); // 8 바이트 
 
 	return 0; // 해당 함수를 정상적으로 끝내고서 운영체제에게 기술한 값을 반환하고 현재 실행 중인 함수를 끝낸다는 뜻
